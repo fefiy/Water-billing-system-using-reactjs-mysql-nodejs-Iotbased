efa92dc3671421b23ac1dc32fdec2cf40794de93
fefiy <foziayimam87@gmail.com>
2023-10-23 12:37:39 +0300

    update connetion to sql

--- server/connect.js
+++ server/connect.js
@@ -1,11 +1,9 @@
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
+const urlDb = `mysql://${process.env.MYSQL_ADDON_USER}:${process.env.MYSQL_ADDON_PASSWORD}@${process.env.MYSQL_ADDON_HOST}:${process.env.MYSQL_ADDON_PORT}/${process.env.MYSQL_ADDON_DB}`
 
-const db = mysql.createConnection({
-  host:process.env.MYSQLHOST,
-  user:process.env.MYSQLUSER,
-  password:process.env.MYSQLPASSWORD,
-  database:process.env.MYSQLDATABASENAME
-})
+const db = mysql.createConnection(urlDb);
 
 db.connect((err) => {
   if (err) {
@@ -15,22 +13,4 @@ db.connect((err) => {
   console.log("Connected to the database");
 });
 
-module.exports = db
-
-
-
-// const mysql = require("mysql");
-// const dotenv = require("dotenv");
-// dotenv.config();
-// const urlDb = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
-// const db = mysql.createConnection(urlDb);
-
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Failed to connect to the database:", err);
-//     return;
-//   }
-//   console.log("Connected to the database");
-// });
-
-// module.exports = db;
+module.exports = db;
